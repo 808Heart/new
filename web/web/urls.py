0395537bbc24edc8242d6af9f15e869738bbfaaf
Achilles, son of Peleus <bigboystar444@gmail.com>
2024-09-15 01:45:05 +0300

    LZ4

--- web/web/urls.py
+++ web/web/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from web import views
-urlpatterns = path ( 'admin/', aclmin. site. urls),
+urlpatterns = [
+path("", views.index),
+path ( 'about' , views. about),
+path ( 'contact', views. contact),
+]
