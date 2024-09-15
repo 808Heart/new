f3caa7922434507319ea6093be6341dfe4128a64
Achilles, son of Peleus <bigboystar444@gmail.com>
2024-09-15 13:53:02 +0300

    LZ6_3

--- web/hello/web/views.py
+++ web/hello/web/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render
 def index(request):
-    data = {"header": "Передача параметров в шаблон Django", "message": "Загружен шаблон templates/firstapp/index_app1.html"}
+# return render(request, "firstapp/home.html")
+    data = {"header": "Передача параметров в шаблон Django","message": "Загружен шаблон templates/firstapp/index_app1.html"}
     return render(request, "web/index_app1.html", context=data)
 def about(request):
     return HttpResponse("About")
