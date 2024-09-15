e12f11ad271e7991db99273325d80e0304e471fd
Achilles, son of Peleus <bigboystar444@gmail.com>
2024-09-15 14:08:46 +0300

    LZ7_1

--- web/hello/web/views.py
+++ web/hello/web/views.py
@@ -1,12 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render
 def index(request):
-    header = "Персональные данные" # обычная переменная
-    langs = ["Английский", "Немецкий", "Испанский"] # массив
-    user = {"name": "Максим,", "age": 30} # словарь
-    addr = ("Виноградная", 23, 45) # кортеж
-    data = {"header": header, "langs": langs, "user": user, "address": addr}
-    return render(request, "index.html", context=data)
+    return render(request, "web/home.html")
 def about(request):
     return HttpResponse("About")
 def contact(request):
